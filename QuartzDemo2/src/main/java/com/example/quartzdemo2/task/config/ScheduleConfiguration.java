365ae0dd1bc72a6c695d01bb7d70f4c31bd07a44
pepsi-wyl <2322535585@qq.com>
2023-11-08 15:33:33 +0800

    Quartz 完善

--- QuartzDemo2/src/main/java/com/example/quartzdemo2/task/config/ScheduleConfiguration.java
+++ QuartzDemo2/src/main/java/com/example/quartzdemo2/task/config/ScheduleConfiguration.java
@@ -21,6 +21,7 @@ public class ScheduleConfiguration {
         @Bean
         public JobDetail firstJobDetail() {
             return JobBuilder.newJob(FirstJob.class)     // 绑定任务
+                    .withDescription("First Job任务描述")
                     .withIdentity("firstJobDetail", "firstJobDetail-Group")  // JobDetail 的唯一标识是 JobKey ，使用 name + group 两个属性
                     .storeDurably()                      // 没有 Trigger 关联时任务是否被保留 < 创建 JobDetail 时，还没 Trigger 指向它，所以需要设置为 true ，表示保留 >
                     .build();
